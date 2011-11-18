1bdfa79e979fc783d6730759a3dba82f090e5c5f
Nathan Walker <nathan.walker@bizbuilt.com>
2011-11-17 21:07:29 -0800

    Initial commit for bee creek cms project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MybeecreekCms::Application.config.secret_token = '38b252ea841f5f02255152a29b65a74f23783024ac89e614af753d71bf051937fd388994de4bb9f8a0299e9dd03213fd966870bf97d2ad2f860bfbc75278cc1f'
